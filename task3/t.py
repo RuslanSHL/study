166b644ffdc9459aa4853a9617b3887cc04f8c94
RuslanSH <ruslansh2009@mail.ru>
2024-12-04 08:00:44 +0700

    Решена задача 3. Перемещенна в папку task3

---
+++ task3/t.py
@@ -0,0 +1,48 @@
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt6.QtGui import QBrush, QColor, QPainter
+from PyQt6.QtCore import Qt
+
+from random import randint
+from sys import argv
+
+
+class Window(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setMouseTracking(True)
+        self.paint = False
+        self.brush = QBrush()
+        self.brush.setStyle(Qt.BrushStyle.SolidPattern)
+        self.setFixedSize(600, 600)
+        self.initUI()
+
+    def paintEvent(self, e):
+        if self.paint:
+            painter = QPainter()
+            painter.begin(self)
+            for _ in range(10):
+                x = randint(0, 600)
+                y = randint(0, 600)
+                size = randint(10, 300)
+                r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+                self.brush.setColor(QColor(r, g, b))
+                painter.setBrush(self.brush)
+                painter.drawEllipse(x, y, size, size)
+            painter.end()
+            self.paint = False
+
+    def initUI(self):
+        self.button = QPushButton(self)
+        self.button.clicked.connect(self.draw)
+        self.button.move(300, 300)
+
+    def draw(self):
+        self.paint = True
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(argv)
+    window = Window()
+    window.show()
+    app.exec()
